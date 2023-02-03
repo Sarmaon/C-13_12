b77f9019ed461c5c6551fd025590dc606901135a
Sarmaon <patratiyaa@gmail.com>
2023-02-03 15:22:42 +0800

    Задача 24

---
+++ Exampl24/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите прогрумму, которая принимает на вход число (А) и выдает суммe чисел от 1 до А
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers}");
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
---
+++ Task17_сoordinats/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 17. Напишите программу, которая принимает на вход координаты точки (х, у)
+//причем Х и У не равны нулю и выдает номер четверит плоскости, в которой находится
+//эта точка.
+
+Console.WriteLine("Введите координаты точки");
+
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Y: ");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+int quater = Quater(x, Y);
+string result = quater > 0 ?  $"Указанные координаты соотвтетствуют четверти => {quater}" 
+                            : "Введены некоректные координаты";              //(? - обозначает ТОГДА) (: - иначе)
+Console.WriteLine(result);
+
+int Quater(int xc, int yc)
+{
+    if(xc > 0 && yc > 0) return 1;
+    if(xc < 0 && yc > 0) return 2;              //&& - обозначение "или"
+    if(xc < 0 && yc < 0) return 3;
+    if(xc > 0 && yc < 0) return 4;
+    return 0;
+}
